a89de5ec5630938e1ef7f7953934ab2a3094b572
Maxim Esin <esinmy@gmail.com>
2020-08-03 15:39:53 +0300

    Added js

---
+++ script.js
@@ -0,0 +1,12 @@
+const toggle = document.querySelector("#toggle");
+const close = document.querySelector("#close");
+const open = document.querySelector("#open");
+const modal = document.querySelector("#modal");
+
+toggle.addEventListener("click", () => document.body.classList.toggle("show-nav"));
+
+open.addEventListener("click", () => modal.classList.add("show-modal"));
+
+close.addEventListener("click", () => modal.classList.remove("show-modal"));
+
+window.addEventListener("click", e => e.target == modal ? modal.classList.remove("show-modal") : false);
